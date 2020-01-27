fe6f96b7807d841d1a74ccbad97bd99b6771b2e9
Austin Pena <autin@equipomedia.com>
2020-01-27 13:35:21 -0800

    Added build folder for debugging

---
+++ build/precache-manifest.864520f4074ecab34ba77ae615763263.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33271d19d5655ccbc2a2aca91e2d93c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621bbc1da13bea3a6b39",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "caf12b02ab685aab55eb",
+    "url": "/static/css/main.6997345f.chunk.css"
+  },
+  {
+    "revision": "621bbc1da13bea3a6b39",
+    "url": "/static/js/2.8b440df0.chunk.js"
+  },
+  {
+    "revision": "89443a3dd5273a163c9d1b817a245857",
+    "url": "/static/js/2.8b440df0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "caf12b02ab685aab55eb",
+    "url": "/static/js/main.d6c6a631.chunk.js"
+  },
+  {
+    "revision": "4861c67555fdd8793b72",
+    "url": "/static/js/runtime-main.75022f1e.js"
+  },
+  {
+    "revision": "e36600ac39f123e71bbbba34bb10850e",
+    "url": "/static/media/angled-decoration.e36600ac.svg"
+  },
+  {
+    "revision": "d23ddf010407437a45e7de850e35d747",
+    "url": "/static/media/background-left.d23ddf01.svg"
+  },
+  {
+    "revision": "6b0e9c2737c2ee640a5330aca8110ba6",
+    "url": "/static/media/exit-mark.6b0e9c27.svg"
+  },
+  {
+    "revision": "ceeaa171dba8506ab4ce31c4bd69d26d",
+    "url": "/static/media/iphone.ceeaa171.png"
+  }
+]);
